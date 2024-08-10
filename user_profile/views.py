af72cc03d6df149c256ffb1d052b38b913d8d0cd
ratulhasan001 <ratulhasan2108@gmail.com>
2024-08-10 21:31:26 +0600

    all

--- user_profile/views.py
+++ user_profile/views.py
@@ -24,8 +24,8 @@ from django.contrib.auth import login
 from django.contrib.sites.shortcuts import get_current_site
 from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
 from django.utils.encoding import force_bytes, force_str
+from django.contrib.auth.tokens import default_token_generator
 from django.core.mail import EmailMessage
-from .tokens import account_activation_token
 from django.contrib.auth.models import User
 from .forms import RegistrationForm
 from django import forms
@@ -66,7 +66,7 @@ class UserRegistrationView(FormView):
             'user': user,
             'domain': current_site.domain,
             'uid': urlsafe_base64_encode(force_bytes(user.pk)),
-            'token': account_activation_token.make_token(user),
+            'token': default_token_generator.make_token(user),
         })
         to_email = form.cleaned_data.get('email')
         email = EmailMessage(mail_subject, message, to=[to_email])
@@ -81,7 +81,7 @@ def activate(request, uidb64, token):
         user = User.objects.get(pk=uid)
     except(TypeError, ValueError, OverflowError, User.DoesNotExist):
         user = None
-    if user is not None and account_activation_token.check_token(user, token):
+    if user is not None and default_token_generator.check_token(user, token):
         user.is_active = True
         user.save()
         login(request, user)
